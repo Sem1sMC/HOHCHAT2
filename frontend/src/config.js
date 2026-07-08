a5260857f721d140d3c7a489a4016f01ec1fee7c
Sem1sMC <semn.novogodniy@mail.ru>
2026-07-09 00:47:53 +0800

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // URL для API
 const API_URL = import.meta.env.PROD 
-const API_URL = 'https://hohchat.onrender.com/api';
+  ? 'https://hohchat.onrender.com/api'
   : 'http://localhost:5000/api';
 
 export { API_URL };
